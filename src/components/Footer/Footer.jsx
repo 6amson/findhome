57786441588b3bc4fd394aec8f5954a2bdf33bac
bunmigrey <bunmigrey@icloud.com>
2023-03-26 18:29:03 +0100

    readjusting the icons

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import './navbar.scss';
+import { useRef, useState, useEffect } from 'react';
+
+export default function Footer() {
+
+    return (
+        <div className='footerDiv'>
+            <div className='footerDivUpper'>
+                <div className='footerUpper1'>
+                    <div className='div7'></div>
+                    <div>
+                        <p>Toke Makinwa</p>
+                        <p>I am glad i contacted findhome for my property,
+                            their customer service is so good and exceptional. Highly recommend
+                        </p>
+                        
+                    </div>
+                </div>
+                <div className='footerUpper2'>
+
+                </div>
+            </div>
+
+            <div className='footerDivLower'>
+
+            </div>
+        </div>
+    )
+}
